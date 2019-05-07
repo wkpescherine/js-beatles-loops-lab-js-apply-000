7077b561bb9a1f519867769abbad9d8467ff4a60
William Pescherine <wkpescherine@gmail.com>
2019-05-07 20:38:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians[],instruments[]){
+  var newstuff[];
+  
+  for(var a = 0; a<=musicians.length; a++){
+    var newWord =musician[a]+ instrument[a];
+    newstuff.push(newWord);
+  }
+  
+  return newstuff
+}
+
+function johnLennonFacts(){
+  while(var a < facts){
+    
+  }
+}

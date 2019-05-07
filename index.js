9193bf1bac2c27d3bcf23031bf9d9350fb95fe90
William Pescherine <wkpescherine@gmail.com>
2019-05-07 20:48:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
-  var newstuff[];
+  var newstuff=[];
   
   for(var a = 0; a<=musicians.length; a++){
     var newWord =musician[a]+ instrument[a];
